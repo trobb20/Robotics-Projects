9804495553f081da999bcfca91574c181902edb1
Teddy <teddyrobbins3@gmail.com>
2022-03-08 13:12:19 -0500

    testbed

---
+++ toothpaste_dispenser/testbed.py
@@ -0,0 +1,47 @@
+from OnshapeSpikePrime.OnshapePlus import *
+
+##
+##
+## Config Client
+
+try:
+    try:
+        exec(open('../apikeys.py').read())
+        base = 'https://cad.onshape.com'
+        client = Client(configuration={"base_url": base,
+                                    "access_key": access,
+                                    "secret_key": secret})
+        print('client configured')
+    except:
+        exec(open('apikeys.py').read())
+        base = 'https://cad.onshape.com'
+        client = Client(configuration={"base_url": base,
+                                    "access_key": access,
+                                    "secret_key": secret})
+        print('client configured')
+except:
+    keyConfig = input('api keys not found, would you like to import keys from a file? [y/n]: ')
+    if keyConfig == "y":
+        root = tk.Tk()
+        root.withdraw()
+        root.update()
+        file_path = filedialog.askopenfilename()
+        exec(open(file_path).read())
+        base = 'https://cad.onshape.com'
+        client = Client(configuration={"base_url": base,
+                                    "access_key": access,
+                                    "secret_key": secret})
+        print('client configured')
+        # root.deiconify()
+        root.update()
+        test = root.destroy()
+        print(test)
+        # root.quit()
+    else:
+        access = input('Please enter your access key: ')
+        secret = input('Please enter your secret key: ')
+        base = 'https://cad.onshape.com'
+        client = Client(configuration={"base_url": base,
+                                    "access_key": access,
+                                    "secret_key": secret})
+        print('client configured')
