7ac383f10ff524f2c1add8d1a87e611064e17656
Teddy <teddyrobbins3@gmail.com>
2022-03-09 11:59:39 -0500

    testing connection and mates

--- toothpaste_dispenser/testbed.py
+++ toothpaste_dispenser/testbed.py
@@ -1,47 +1,20 @@
 from OnshapeSpikePrime.OnshapePlus import *
 
-##
-##
-## Config Client
-
-try:
+def configure_client(api_path, base='https://cad.onshape.com'):
     try:
-        exec(open('../apikeys.py').read())
-        base = 'https://cad.onshape.com'
+        access, secret = open(api_path).readlines()
         client = Client(configuration={"base_url": base,
-                                    "access_key": access,
+                                    "access_key": access.strip('\n'),
                                     "secret_key": secret})
         print('client configured')
-    except:
-        exec(open('apikeys.py').read())
-        base = 'https://cad.onshape.com'
-        client = Client(configuration={"base_url": base,
-                                    "access_key": access,
-                                    "secret_key": secret})
-        print('client configured')
-except:
-    keyConfig = input('api keys not found, would you like to import keys from a file? [y/n]: ')
-    if keyConfig == "y":
-        root = tk.Tk()
-        root.withdraw()
-        root.update()
-        file_path = filedialog.askopenfilename()
-        exec(open(file_path).read())
-        base = 'https://cad.onshape.com'
-        client = Client(configuration={"base_url": base,
-                                    "access_key": access,
-                                    "secret_key": secret})
-        print('client configured')
-        # root.deiconify()
-        root.update()
-        test = root.destroy()
-        print(test)
-        # root.quit()
-    else:
-        access = input('Please enter your access key: ')
-        secret = input('Please enter your secret key: ')
-        base = 'https://cad.onshape.com'
-        client = Client(configuration={"base_url": base,
-                                    "access_key": access,
-                                    "secret_key": secret})
-        print('client configured')
+    except Exception as e:
+        print(e)
+        return
+
+    return client
+
+base = 'https://cad.onshape.com'
+url = 'https://cad.onshape.com/documents/e61d2284326681e60c354303/w/06c3440ad86b926c4800c9f0/e/4a24c86674282951b16e2dcf'
+client = configure_client('OnshapeSpikePrime/apikeys.txt')
+
+mates = getMates(client, url, base)
