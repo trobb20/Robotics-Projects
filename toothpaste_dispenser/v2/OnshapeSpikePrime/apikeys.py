5eb565f0209b74ccf194b60fc29077e50043d8ea
trobb20 <teddyrobbins3@gmail.com>
2022-03-13 17:33:43 -0400

    Delete apikeys.py

--- toothpaste_dispenser/v2/OnshapeSpikePrime/apikeys.py
+++
@@ -1,3 +0,0 @@
-base = 'https://cad.onshape.com' ## only need to specify base if different than "https://cad.onshape.com"
-access = '6GVsfOwpGEkQ9xj5uCFvYLlN'
-secret = 'Asl8QGRLquWLnznPARuJm8F1OkviRWWXO8BHxz8OLoHWzYeS'
