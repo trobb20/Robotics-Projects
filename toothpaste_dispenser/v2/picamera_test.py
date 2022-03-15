9a516205dd7828f1987fb5f95f6ae9134160a41b
Teddy <teddyrobbins3@gmail.com>
2022-03-15 17:28:17 -0400

    testing opencv for switch in toothpaste detection

--- toothpaste_dispenser/v2/picamera_test.py
+++ toothpaste_dispenser/v2/picamera_test.py
@@ -4,30 +4,25 @@ import time
 
 cap = cv.VideoCapture(0)
 
-
 def mask_magnitude(mask):
     mask_magnitude = np.sum(mask) / (mask.shape[0] * mask.shape[1]) * (100 / 255)
     return mask_magnitude
 
-
 lower = np.array([0, 0, 191])
 upper = np.array([180, 255, 255])
 
-try:
-    while True:
-        # Take each frame
-        _, frame = cap.read()
-        hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
-        mask = cv.inRange(hsv, lower, upper)
-        mag = mask_magnitude(mask)
-        print('Magnitude is %s'%str(mag))
+while True:
+    # Take each frame
+    _, frame = cap.read()
+    hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+    mask = cv.inRange(hsv, lower, upper)
+    mag = mask_magnitude(mask)
+    print('Magnitude is %s'%str(mag))
 
-        cv.imshow('frame', frame)
-        cv.imshow('mask', mask)
+    cv.imshow('frame', frame)
+    cv.imshow('mask', mask)
 
-        time.sleep(.25)
+    time.sleep(.25)
 
-except KeyboardInterrupt:
-    exit()
 
 
